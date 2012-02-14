82933e0d10bc60534c47e0056725feb50fbd666e
Ross Paffett <ross@rosspaffett.com>
2012-02-13 22:42:10 -0500

    Initial commit

---
+++ src/main/java/com/stratomine/bukkit/plugins/stats/StatsPlugin.java
@@ -0,0 +1,31 @@
+package com.stratomine.bukkit.plugins.stats;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StatsPlugin extends JavaPlugin {
+	
+	public void onEnable() {
+		log("Hello world!");
+	}
+	
+	public void onDisable() {
+		// Do nothing
+	}
+	
+	protected Logger getLogger() {
+		return getServer().getLogger();
+	}
+	
+	public void log(Level level, String message) {
+		message = "[" + getDescription().getName() + "] " + message;
+		getLogger().log(level, message);
+	}
+	
+	public void log(String message) {
+		log(Level.INFO, message);
+	}
+	
+}
---
+++ src/test/java/com/stratomine/bukkit/plugins/stats/StatsPluginTest.java
@@ -0,0 +1,14 @@
+package com.stratomine.bukkit.plugins.stats;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StatsPluginTest {
+	
+	@Test
+	public void testPlugin() {
+		fail("Not yet implemented");
+	}
+	
+}
